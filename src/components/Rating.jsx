d2ea2ad744f8b092ed4b2e9969582df17ba1312d
Patrick <pfkellogg@gmail.com>
2025-12-16 18:30:18 -0500

    lists

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,7 +1,18 @@
 const Rating = () => {
+  const stars = Array.from({ length: 5 }, (_, i) => i + 1)
+  console.log(stars)
+
   return (
     <div className="rating-container">
       <h2>Rate Your Experience</h2>
+      <div className="stars">
+        {/* {[1, 2, 3, 4, 5].map((star, index) => ( */}
+        {stars.map((star, index) => (
+          <span key={star} className="star">
+            {'\u2605'}
+          </span>
+        ))}
+      </div>
     </div>
   )
 }
