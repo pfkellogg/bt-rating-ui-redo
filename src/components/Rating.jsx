80689596dffb48887abb307610d9944557567c45
Patrick <pfkellogg@gmail.com>
2025-12-16 11:37:48 -0500

    styling-in-react

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,4 +1,18 @@
 const Rating = () => {
-  return <div>Rate Your Experience</div>
+  return (
+    <div className="rating-container">
+      <h2>Rate Your Experience</h2>
+    </div>
+  )
 }
+
+// const styles = {
+//   container: {
+//     textAlign: 'left',
+//     fontFamily: 'Arial',
+//     padding: '20px'
+//   },
+//   heading: { color: 'red' }
+// }
+
 export default Rating
