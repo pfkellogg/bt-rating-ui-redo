0c9554ba06d52c3adff0ede534bab87253a96ad4
Patrick <pfkellogg@gmail.com>
2025-12-16 10:16:57 -0500

    creating-components-redo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import Rating from './components/Rating'
+
 const App = () => {
-  return <div>Rate Your Experience</div>
+  const name = 'Patrick'
+  return (
+    <div>
+      {/* <h1>Hello {name}</h1>
+      <h1>Hello {1 + 1}</h1> */}
+      <Rating />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Rating.jsx
@@ -0,0 +1,4 @@
+const Rating = () => {
+  return <div>Rate Your Experience</div>
+}
+export default Rating
