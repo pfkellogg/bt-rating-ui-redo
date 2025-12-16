28289cb80890c84baafb0a8390e84251651c1418
Patrick <pfkellogg@gmail.com>
2025-12-16 18:52:19 -0500

    handling events

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,13 +2,20 @@ const Rating = () => {
   const stars = Array.from({ length: 5 }, (_, i) => i + 1)
   console.log(stars)
 
+  const clicked = index => console.log('clicked', index)
+  const hovered = (direction, index) => console.log('Hovered!', direction, index)
+
   return (
     <div className="rating-container">
       <h2>Rate Your Experience</h2>
       <div className="stars">
         {/* {[1, 2, 3, 4, 5].map((star, index) => ( */}
         {stars.map((star, index) => (
-          <span key={star} className="star">
+          // this fires off immediately because of the parentheses
+          // <span onClick={clicked(index)} key={star} className="star">
+
+          // this works because of the arrow function
+          <span onClick={() => clicked(index)} onMouseEnter={() => hovered('enter', index)} onMouseLeave={() => hovered('leave', index)} key={star} className="star">
             {'\u2605'}
           </span>
         ))}
