35c44098d10900fa890bdc85b89efac5591ed660
Patrick <pfkellogg@gmail.com>
2025-12-17 09:58:47 -0500

    rating state

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,6 +1,15 @@
+import { useState } from 'react'
+// useState is a function
+// console.log(useState, typeof useState)
+
 const Rating = () => {
+  const [rating, setRating] = useState(0)
+  const [hover, setHover] = useState(0)
+
+  console.log('Rating value:', rating, 'Type of rating:', typeof rating)
+
   const stars = Array.from({ length: 5 }, (_, i) => i + 1)
-  console.log(stars)
+  console.log(stars, 'Type of stars:', typeof stars)
 
   const clicked = index => console.log('clicked', index)
   const hovered = (direction, index) => console.log('Hovered!', direction, index)
@@ -8,14 +17,15 @@ const Rating = () => {
   return (
     <div className="rating-container">
       <h2>Rate Your Experience</h2>
+      <p>Hover: {hover}</p>
+      <p>Rating: {rating}</p>
       <div className="stars">
-        {/* {[1, 2, 3, 4, 5].map((star, index) => ( */}
         {stars.map((star, index) => (
           // this fires off immediately because of the parentheses
           // <span onClick={clicked(index)} key={star} className="star">
 
           // this works because of the arrow function
-          <span onClick={() => clicked(index)} onMouseEnter={() => hovered('enter', index)} onMouseLeave={() => hovered('leave', index)} key={star} className="star">
+          <span onClick={() => setRating(star)} onMouseEnter={() => setHover(star)} onMouseLeave={() => setHover(0)} key={star} className="star">
             {'\u2605'}
           </span>
         ))}
